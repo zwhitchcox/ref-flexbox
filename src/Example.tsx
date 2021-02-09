3fa694a8b25c0db5495d3056f144fa2654ef05f3
Zane Hitchcox <zane@balena.io>
2021-02-09 18:15:55 -0500

    init

---
+++ src/Example.tsx
@@ -0,0 +1,13 @@
+import "./Example.scss"
+const Example = () => {
+  return (
+    <div className="flex-example">
+      <label>
+        <span />
+        <input />
+      </label>
+    </div>
+  )
+}
+
+export default Example
